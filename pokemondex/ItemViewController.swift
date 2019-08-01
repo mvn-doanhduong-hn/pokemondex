ddf74c356ba37dd94fe3378bb7db33275a0ad740
DoanhDD <doanhdd@gmail.com>
2019-08-01 17:32:04 +0700

    Layout for Pokemon screen

---
+++ pokemondex/ItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemViewController.swift
+//  pokemondex
+//
+//  Created by MacMini2012 on 8/1/19.
+//  Copyright © 2019 Pokemondex. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- pokemondex/PokemonViewController.swift
+++ pokemondex/PokemonViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class PokemonViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

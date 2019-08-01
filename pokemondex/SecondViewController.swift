c3804ae21d7d6adb9abd2ebefd504b5ce917281d
DoanhDD <doanhdd@gmail.com>
2019-08-01 15:10:11 +0700

    Initial Commit

---
+++ pokemondex/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  pokemondex
+//
+//  Created by MacMini2012 on 8/1/19.
+//  Copyright © 2019 Pokemondex. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
